5f17b9c35226ee27238d0a76a3e7839d57f58f5a
Alex Gonzalez <alexgonrod83@gmail.com>
2024-10-22 23:11:09 +0200

    adding netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon({
@@ -10,5 +12,7 @@ export default defineConfig({
       mdi: ["*"],
     },
   })],
+
   output: "hybrid",
+  adapter: netlify(),
 });
