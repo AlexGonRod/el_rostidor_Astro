45a9ee456f035510039714d387c9a0abbb90bb90
Alex Gonzalez <alexgonrod83@gmail.com>
2024-10-31 20:27:57 +0100

    ig images endpoint

---
+++ src/pages/api/images.json.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+export const prerender = false;
+
+const token = import.meta.env.IG_TOKEN ?? "";
+const url = import.meta.env.IG_URL ?? "";
+
+export const GET: APIRoute = async () => {
+    const response = await fetch(url + token)
+    const {data} = await response.json()
+    return new Response(JSON.stringify({
+        images: data,
+        message: "IG images OK"
+    }))
+}
