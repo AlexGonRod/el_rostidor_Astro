3178cc9b22c3a0625ea19865e2f26203f1537c44
Alex Gonzalez <alexgonrod83@gmail.com>
2024-11-13 19:55:17 +0100

    fetch images from API

--- src/pages/api/images.json.ts
+++ src/pages/api/images.json.ts
@@ -1,5 +1,6 @@
 import type { APIRoute } from "astro";
 export const prerender = false;
+const _url = "https://graph.instagram.com/v21.0/"
 
 const token = import.meta.env.IG_TOKEN ?? "";
 const url = import.meta.env.IG_URL ?? "";
@@ -7,6 +8,7 @@ const url = import.meta.env.IG_URL ?? "";
 export const GET: APIRoute = async () => {
     const response = await fetch(url + token)
     const {data} = await response.json()
+
     return new Response(JSON.stringify({
         images: data,
         message: "IG images OK"
