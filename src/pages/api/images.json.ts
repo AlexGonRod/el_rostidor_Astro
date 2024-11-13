c2cefe47ba7429c15667f948ab32c97dd7f1349f
Alex Gonzalez <alexgonrod83@gmail.com>
2024-11-13 20:39:59 +0100

    again with variables

--- src/pages/api/images.json.ts
+++ src/pages/api/images.json.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 export const prerender = false;
 
-const token = import.meta.env.IG_TOKEN ?? "";
+const token = import.meta.env.IG_TOKEN ?? ""
 const url = import.meta.env.IG_URL ?? "";
 
 export const GET: APIRoute = async () => {
