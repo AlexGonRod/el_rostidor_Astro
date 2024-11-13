c076c399257d0d3ab3f77c4b2654d42db35cab5f
Alex Gonzalez <alexgonrod83@gmail.com>
2024-11-13 20:11:37 +0100

    fix deploy

--- src/pages/api/images.json.ts
+++ src/pages/api/images.json.ts
@@ -1,6 +1,5 @@
 import type { APIRoute } from "astro";
 export const prerender = false;
-const _url = "https://graph.instagram.com/v21.0/"
 
 const token = import.meta.env.IG_TOKEN ?? "";
 const url = import.meta.env.IG_URL ?? "";
