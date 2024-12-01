b5c3ad4c5f4c947a443e32e2879c1d52ff3ca470
Alex Gonzalez <alexgonrod83@gmail.com>
2024-12-01 23:55:52 +0100

    delete console.log

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -3,7 +3,6 @@ import spanish from './es.json'
 
 
 export const geti18n = ({pathname = 'ca'}: {pathname: string | undefined}) =>{
-    console.log('hola: ', pathname)
     if(pathname === "/es") return spanish
     if(pathname === '') return catalan
     return catalan
